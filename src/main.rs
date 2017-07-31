dfc09da0823eab64d3d77fc8fbedbc059279cf4b
harrison <notallama@gmail.com>
2017-07-31 15:17:15 -0400

    rand unit fix

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,33 @@ fn sd_sphere(p:V3,r:f32) -> f32 {
 	p.mag()-r
 }
 
+fn union<T>(a:(f32,T), b:(f32,T)) -> (f32,T) {
+	if a.0 < b.0 {
+		a
+	}
+	else {
+		b
+	}
+}
+
 fn scene(p:V3) -> (f32,V3) {
-	let d = sd_sphere(p-V3(0.0,0.0,0.0), 1.0);
-	let c = V3(0.3,0.5,0.2);
-	(d,c)
+	let s0 = sd_sphere(p-V3(1.0,0.0,0.0), 1.0);
+	let c0 = V3(0.3,0.5,0.2);
+
+	let o0 = (s0,c0);
+
+
+	let s1 = sd_sphere(p-V3(-0.75,0.0,0.0), 0.75);
+	let c1 = V3(0.8,0.2,0.2);
+
+	let o1 = (s1,c1);
+
+
+	union(o0, o1)
 }
 
 fn sky(d:V3) -> V3 {
+	let _ = d;
 	V3::new(1.0)
 }
 
@@ -37,21 +57,21 @@ struct SceneHit {
 	normal : V3,
 	col : V3,
 }
-fn trace(o:V3,d:V3)->Option<SceneHit> {
-	let mut p = o;
-	let mut sd = 1000.0;
+fn trace(o:V3,d:V3, max_dist:f32)->Option<SceneHit> {
+	let mut dist = 0.0;
+	let mut sd = 0.0;
 
-	for iter in 0..64 {
-		sd = scene(p).0;
-		p = p+d*sd;
-		if sd < 0.00001 {
+	for _ in 0..64 {
+		sd = scene(o+d*dist).0;
+		dist += sd;
+		if dist > max_dist || sd < 0.00001 {
 			break;
 		}
 	}
 
-	p = p - d*0.0001;
-	let hit = sd < 0.0001;
-
+	dist -= 0.0001;
+	let hit = sd < 0.01;
+	let p = o+d*dist;
 	if hit {
 		Some(SceneHit {
 			pos : p,
@@ -68,9 +88,8 @@ fn trace_bounces(rng : &Rng, mut o:V3,mut d:V3)->V3 {
 	let bounces = 10;
 
 	let mut c = V3::new(1.0);
-	let mut mult = 1.0;
 	for i in 0..bounces {
-		match trace(o,d) {
+		match trace(o,d, 1000.0) {
     		Some(SceneHit{pos,normal,col}) => {
     			let l = rng.sub(i).unit();
     			let l = if normal.dot(l) < 0.0 { -l } else { l };
@@ -79,45 +98,68 @@ fn trace_bounces(rng : &Rng, mut o:V3,mut d:V3)->V3 {
     			d = l;
     		},
     		None => {
-    			break;
+    			return c*sky(d);
     		}
    		};
 	}
 
-	c*sky(d)
+	V3::new(0.0)
+}
 
+fn monte_carlo(rng : &Rng, o:V3, d:V3) -> V3 {
+	let mut col = V3::new(0.0);
+
+	let iters = 50;
+	let norm = 1.0 / (iters as f32);
+	for i in 0..iters {
+		col = col + trace_bounces(&rng.sub(i),o,d) * norm;
+	}
+	col
+}
+
+fn get_color(rng : &Rng, o:V3, d:V3) -> V3 {
+	let light_pos = V3(1.0, 1.0, -2.0);
+	match trace(o,d, 1000.0) {
+		Some(hit) => {
+			let light_diff = light_pos-hit.pos;
+			let light_dist = light_diff.mag();
+			let light_dir = light_diff/light_dist;
+			match trace(hit.pos, light_dir, light_dist) {
+				Some(_) => V3::new(0.0),
+				None => hit.col * light_dir.dot(hit.normal) / (light_dist*light_dist),
+			}
+		}
+		None => sky(d)
+	}
 }
 
 fn main() {
     let mut file = File::create("out.png").unwrap();
 
-    let width = 256;
-    let height = 256;
+    let width = 800;
+    let height = 800;
 
     let mut frame = Vec::with_capacity(width*height*4);
     frame.resize(width*height*4, 0);
 
     let cam = V3(0.0,0.0,-2.0);
-    let rng = Rng::new(0);
+
+    let rng = Rng::new(1); // <--- CREATE
+
     for j in 0..height {
-    	let rng = rng.sub(j);
+    	let rng = rng.sub(j); // <--- SPLIT
+
     	for i in 0..width {
-    		let rng = rng.sub(i);
+    		let rng = rng.sub(i); // <--- SPLIT
+
+    		//calculate camera ray
     		let pos = V3(i as f32, j as f32, 0.0) / V3(width as f32, height as f32, 1.0) * 2.0 - 1.0;
     		let dir = (pos-cam).unit();
-    		//let col = match trace(cam,dir) {
-    		//	Some(SceneHit{pos,normal,col}) => {
-    		//		col
-    		//	},
-    		//	None => V3::new(0.0)
-    		//;
 
-    		let mut col = V3::new(0.0);
-    		let ray_count = 100;
-    		for k in 0..ray_count {
-    			let rng = rng.sub(k);
-    			col = col + trace_bounces(&rng, pos,dir) / (ray_count as f32);
-    		}
+    		//trace ray
+    		let col = monte_carlo(&rng, pos, dir); // <--- USE
+
+    		//write pixel to image
     		frame[(j*width+i)*4+0] = (col.0 * 256.0).min(255.0).max(0.0) as u8;
     		frame[(j*width+i)*4+1] = (col.1 * 256.0).min(255.0).max(0.0) as u8;
     		frame[(j*width+i)*4+2] = (col.2 * 256.0).min(255.0).max(0.0) as u8;
--- src/rnd.rs
+++ src/rnd.rs
@@ -1,5 +1,6 @@
 use std;
 use vec::*;
+use std::ops::Range;
 
 fn hash(cur : u64, idx : u64) -> u64 {
 	let c = 0xf46053d10d8c49f5_u64;	
@@ -23,6 +24,11 @@ impl Rng {
 		v = hash(v,0xf2896bf886621af5);
 		Rng(v)
 	}
+
+	pub fn sub(&self, idx : usize) -> Rng {
+		Rng(hash(self.0, (idx as u64) | (2<<57)))
+	}
+
 	pub fn u32(&self) -> u32 {
 		self.0 as u32
 	}
@@ -41,8 +47,11 @@ impl Rng {
 		f - 3.0
 	}
 
-	pub fn sub(&self, idx : usize) -> Rng {
-		Rng(hash(self.0, (idx as u64) | (2<<57)))
+	pub fn get<T:Rand>(&self) -> T {
+		T::gen(&self)
+	}
+	pub fn range<T:RandRange>(&self, r:Range<T>) -> T {
+		T::range(&self, r)
 	}
 
 	pub fn ubox<T:V>(&self) ->T {
@@ -75,8 +84,108 @@ impl Rng {
 		v
 	}
 
-	pub fn unit<T:V>(&self) -> T {
-		self.gaussian::<T>().unit()
+	pub fn unit<T:RandUnit>(&self) -> T {
+		T::unit(&self)
+	}
+}
+
+pub trait Rand {
+	fn gen(rng:&Rng) -> Self;
+}
+pub trait RandRange : Sized {
+	fn range(rng:&Rng, r: Range<Self>) -> Self;
+}
+
+impl Rand for u32 {
+	fn gen(rng:&Rng) -> Self {
+		rng.u32()
+	}
+}
+impl RandRange for u32{
+	fn range(rng:&Rng, r: Range<Self>) -> Self {
+		let len = r.end-r.start;
+		let bins = 0xFFFFFFFF / len;
+		let max_valid = bins*len;
+
+		let mut x = 0;
+		for i in 0.. {
+			x = rng.sub(i).u32();
+			if x < max_valid { break; }
+		}
+		x%len + r.start
+	}
+}
+
+impl Rand for u64 {
+	fn gen(rng:&Rng) -> Self {
+		let low = rng.sub(0).u32() as u64;
+		let hi = rng.sub(1).u32() as u64;
+		low | (hi << 32)
+	}
+}
+
+impl RandRange for u64 {
+	fn range(rng:&Rng, r: Range<Self>) -> Self {
+		let len = r.end-r.start;
+		let bins = 0xFFFFFFFFFFFFFFFF / len;
+		let max_valid = bins*len;
+
+		let mut x : u64 = 0;
+		for i in 0.. {
+			x = rng.sub(i).get();
+			if x < max_valid { break; }
+		}
+		x%len + r.start
+	}
+}
+
+impl Rand for usize {
+	#[cfg(target_pointer_width = "32")]
+	fn gen(rng:&Rng) -> Self {
+		rng.u32() as usize
+	}
+
+	#[cfg(target_pointer_width = "64")]
+	fn gen(rng:&Rng) -> Self {
+		let v : u64 = rng.get();
+		v as usize
+	}
+}
+
+impl RandRange for usize {
+	#[cfg(target_pointer_width = "32")]
+	fn range(rng:&Rng, r:Range<Self>) -> Self {
+		rng.range((r.start as u32)..(r.end as u32)) as usize
+	}
+
+	#[cfg(target_pointer_width = "64")]
+	fn range(rng:&Rng, r:Range<Self>) -> Self {
+		rng.range((r.start as u64)..(r.end as u64)) as usize
+	}
+}
+
+impl Rand for f32 {
+	fn gen(rng:&Rng) -> Self {
+		rng.uval()
+	}
+}
+
+
+pub trait RandUnit {
+	fn unit(rng:&Rng) -> Self;
+}
+
+impl RandUnit for V3 {
+	fn unit(rng:&Rng) -> Self {
+		let theta = rng.sub(0).uval() * 2.0*3.14195;
+		let (x,y) = theta.sin_cos();
+		let r2 = rng.sub(1).uval();
+		let r = r2.sqrt();
+		let (x,y) = (x*r,y*r);
+		let s = (1.0-r2).sqrt();
+
+
+		V3(2.0*x*s,2.0*y*s,1.0-2.0*r2)
 	}
 }
 
